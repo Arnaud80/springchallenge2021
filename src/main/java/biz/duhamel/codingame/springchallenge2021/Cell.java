e36991f3e24cab394297ec9ca5458f420b0280dd
ARNAUD <arnaud@duhamel.biz>
2021-05-09 15:30:04 +0200

    Project initialization

---
+++ src/main/java/biz/duhamel/codingame/springchallenge2021/Cell.java
@@ -0,0 +1,17 @@
+package biz.duhamel.codingame.springchallenge2021;
+
+public class Cell {
+    private int index;
+    private int richness;
+    private int[] neights = new int[5];
+
+    public Cell(int index, int richness, int[] neights) {
+        this.index = index;
+        this.richness = richness;
+        this.neights = neights;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+}
---
+++ src/test/java/biz/duhamel/codingame/springchallenge2021/PlayerTest.java
@@ -0,0 +1,20 @@
+package biz.duhamel.codingame.springchallenge2021;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PlayerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
